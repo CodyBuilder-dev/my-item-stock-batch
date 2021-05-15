c1f5e3cc73c734a793ea2b5a110beb14be5c3d6e
Codybuilder-dev <imspecial1@u.sogang.ac.kr>
2021-05-16 02:27:35 +0900

    화면단 추가

--- src/main/java/com/example/myitemstockbatch/quartz/job/CronJob.java
+++ src/main/java/com/example/myitemstockbatch/quartz/job/CronJob.java
@@ -19,12 +19,13 @@ public class CronJob extends QuartzJobBean {
     //todo: execute와 executeInternal의 차이는 무엇일까?
     protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
         JobDataMap jobDataMap = context.getJobDetail().getJobDataMap();
-        int jobId = jobDataMap.getInt("jobId");
+//        int jobId = jobDataMap.getInt("jobId");
         JobKey jobKey = context.getJobDetail().getKey();
 
         currThread = Thread.currentThread();
         log.info("============================================================================");
-        log.info("CronJob started :: sleep : {} jobId : {} jobKey : {} - {}", MAX_SLEEP_IN_SECONDS, jobId, jobKey, currThread.getName());
+//        log.info("CronJob started :: sleep : {} jobId : {} jobKey : {} - {}", MAX_SLEEP_IN_SECONDS, jobId, jobKey, currThread.getName());
+        log.info("CronJob started :: sleep : {} jobKey : {} - {}", MAX_SLEEP_IN_SECONDS, jobKey, currThread.getName());
 
         IntStream.range(0, 10).forEach(i -> {
             log.info("CronJob Counting - {}", i);
