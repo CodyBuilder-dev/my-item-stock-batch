118ee0425466f5d5fc848e70655ca46251cc530f
Codybuilder-dev <imspecial1@u.sogang.ac.kr>
2021-05-16 03:31:32 +0900

    FORM PUT,DELETE 처리를 위한 HiddenHttpMethodFilter 추가

---
+++ src/main/java/com/example/myitemstockbatch/config/FilterConfig.java
@@ -0,0 +1,24 @@
+package com.example.myitemstockbatch.config;
+
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.Filter;
+import java.util.Arrays;
+
+@Configuration
+public class FilterConfig implements WebMvcConfigurer {
+
+    @Bean
+    public FilterRegistrationBean<? extends Filter> getFilterRegistrationBean() {
+        FilterRegistrationBean<? extends Filter> registrationBean = new FilterRegistrationBean<>(new HiddenHttpMethodFilter());
+        registrationBean.setOrder(Integer.MIN_VALUE);
+        registrationBean.addUrlPatterns("/*");
+//        registrationBean.setUrlPatterns(Arrays.asList("/*"));
+        return registrationBean;
+    }
+}
