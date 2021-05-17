09282a2266e0c73b616cb7ee2c09a6efb77409d9
Codybuilder-dev <imspecial1@u.sogang.ac.kr>
2021-05-17 12:48:43 +0900

    DB 제거후 테스트

--- src/main/java/com/example/myitemstockbatch/quartz/config/DataLoader.java
+++ src/main/java/com/example/myitemstockbatch/quartz/config/DataLoader.java
@@ -1,11 +1,9 @@
-package com.example.myitemstockbatch.quartz.dao;
-
+package com.example.myitemstockbatch.quartz.config;
 
 import com.example.myitemstockbatch.admin.dto.JobRequest;
 import com.example.myitemstockbatch.admin.service.ScheduleService;
 import com.example.myitemstockbatch.quartz.job.CronJob;
 import com.example.myitemstockbatch.quartz.job.SimpleJob;
-
 import org.quartz.JobDataMap;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
@@ -24,22 +22,22 @@ public class DataLoader implements ApplicationListener<ContextRefreshedEvent> {
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
-        //simple job 생성
-        JobRequest jobRequest = new JobRequest();
-        jobRequest.setJobName("simpleJob");
-        jobRequest.setStartDateAt(LocalDateTime.now());
-        jobRequest.setRepeatCount(50);
-        jobRequest.setRepeatIntervalInSeconds(30);
-        scheduleService.addJob(jobRequest, SimpleJob.class);
-
-        //cron job 생성
-        JobDataMap jobDataMap = new JobDataMap();
-        jobDataMap.put("jobId", "123456789");
-        jobRequest = new JobRequest();
-        jobRequest.setJobName("cronJob1");
-        jobRequest.setCronExpression("0 * * ? * *"); //every min
-        jobRequest.setJobDataMap(jobDataMap);
-        scheduleService.addJob(jobRequest, CronJob.class);
+//        //simple job 생성
+//        JobRequest jobRequest = new JobRequest();
+//        jobRequest.setJobName("simpleJob");
+//        jobRequest.setStartDateAt(LocalDateTime.now());
+//        jobRequest.setRepeatCount(50);
+//        jobRequest.setRepeatIntervalInSeconds(30);
+//        scheduleService.addJob(jobRequest, SimpleJob.class);
+//
+//        //cron job 생성
+//        JobDataMap jobDataMap = new JobDataMap();
+//        jobDataMap.put("jobId", "123456789");
+//        jobRequest = new JobRequest();
+//        jobRequest.setJobName("cronJob1");
+//        jobRequest.setCronExpression("0 * * ? * *"); //every min
+//        jobRequest.setJobDataMap(jobDataMap);
+//        scheduleService.addJob(jobRequest, CronJob.class);
 
 //        jobRequest = new JobRequest();
 //        jobRequest.setJobName("cronJob2");
